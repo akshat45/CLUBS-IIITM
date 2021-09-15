92fe7275348eeec46f4df0e969003728f32ed90e
Chaitanyab2001 <chaitanyabulusu2001@gmail.com>
2021-09-15 19:18:26 +0530

    Updated models, controllers and routes of students

--- routes/students.js
+++ routes/students.js
@@ -1,1 +1,21 @@
-import express from ("express");
+const express = require("express");
+const router = express.Router();
+const students = require('../controllers/students');
+
+
+router.route('/')
+   .get(students.index)
+   .post(students.createStudent);
+
+
+router.get('/new', students.renderNewForm);
+
+router.route('/:id')
+    .get(students.showStudent)
+    .put(students.updateStudent)
+    .delete(students.deleteStudent);
+
+router.get('/:id/edit', students.renderEditForm);
+
+module.exports = router;
+

49654485e486817d20d9604a444cde610363af10
Jay Raikhere <jayraykhere@gmail.com>
2021-09-19 13:20:32 +0530

    add club route

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import findOrCreate from "mongoose-findorcreate";
-import session from "express-session";
-import passport from "passport";
-import Googlepassport from "passport-google-oauth20";
 
-import dotenv from "dotenv";
-dotenv.config();
 import { username, password } from "./credentials.js";
 
 import clubRouter from "./routes/clubs.js";
 import eventRouter from "./routes/events.js";
-import studentRouter from "./routes/students.js";
 
+import findOrCreate from "mongoose-findorcreate";
 
+import  session from "express-session";
+import passport from "passport";
+
+import Googlepassport from "passport-google-oauth20";
 const GoogleStrategy = Googlepassport.Strategy;
-const app = express();
 
+const app = express();
 app.use(cors());
+
 app.use(express.json({ limit: "30mb", extended: true })) 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(session({
-    secret: process.env.secret,
+    secret: 'Our little secret.',
     resave: false,
     saveUninitialized: false,
   }));
@@ -31,9 +32,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
 app.use("/club", clubRouter);
 app.use("/event", eventRouter);
-app.use("/student", studentRouter);
 
 const CONNECTION_URL = `mongodb+srv://${username}:${password}@clubsiiitm.awqoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
@@ -41,6 +43,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+    // mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
         googleId: String
@@ -50,6 +53,8 @@ userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
+// passport.use(User.createStrategy());
+ 
 // use static serialize and deserialize of model for passport session support
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -61,7 +66,6 @@ passport.serializeUser(function(user, done) {
     });
   });
 
-  
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID ,
     clientSecret: process.env.CLIENT_SECRET,
@@ -69,7 +73,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
-      // console.log(profile.emails[0].value);
+      console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -77,9 +81,7 @@ passport.use(new GoogleStrategy({
 ));
 
 app.get("/auth/google",
-  passport.authenticate('google', { scope: ["profile"] ,
-  scope:["email"]
-}));
+  passport.authenticate('google', { scope: ["profile"] }));
 
 app.get("/auth/google/club", 
   passport.authenticate('google', { failureRedirect: '/login' }),

2ffe0c220a4c88aff2e7f6e2cb3c62188b5e6f67
Akshat Gupta <akshatg450@gmail.com>
2021-10-17 22:44:40 +0530

    done changes in routes folder

--- routes/club.js
+++ routes/club.js
@@ -1,14 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import nodemailer from "nodemailer";
 import clubModel from "../models/clubs.js";
+import imageUpload from "../middleware/imageUpload.js";
 import { getClub, putClub, removeMember, getJoinButton, getVerifyPresident } from "../controllers/clubs.js";
 import { postEvent } from "../controllers/events.js";
 import { getClubApprovals, postApproval } from "../controllers/approvals.js";
-import { storage } from "../cloudinary/index.js";
-import multer from 'multer';
 
 const router = express.Router();
-const upload = multer({ storage });
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.usern,
+        pass: process.env.passw
+    }
+});
 
 router.get("/:clubId", async function (req,res, next) {
 
@@ -19,37 +26,35 @@ router.get("/:clubId", async function (req,res, next) {
 
     switch ("[object Error]") {
         case Object.prototype.toString.call(club):
-            if ((club.status) < 500)
-            res.status(club.status).send(club.message);
-            else
-            next(club.message);
+            res.status(club.status)
+            req.flash("message", club.message );
+            req.flash("status", club.status);
+            res.redirect("/home");
             break;
 
         case Object.prototype.toString.call(approvals):
-            if ((approvals.status) < 500)
-            res.status(approvals.status).send(approvals.message);
-            else
-            next(approvals.message);
+            res.status(approvals.status)
+            req.flash("message", approvals.message );
+            req.flash("status", approvals.status);
+            res.redirect("/home");
             break;
 
         case Object.prototype.toString.call(joinbutton):
-            if ((joinbutton.status) < 500)
-            res.status(joinbutton.status).send(joinbutton.message);
-            else
-            next(joinbutton.message);
+            res.status(joinbutton.status)
+            req.flash("message", joinbutton.message );
+            req.flash("status", joinbutton.status);
+            res.redirect("/home");
             break;
         
         case Object.prototype.toString.call(verifypresident):
-            if ((verifypresident.status) < 500)
-            res.status(verifypresident.status).send(verifypresident.message);
-            else
-            next(verifypresident.message);
+            res.status(verifypresident.status)
+            req.flash("message", verifypresident.message );
+            req.flash("status", verifypresident.status);
+            res.redirect("/home");
             break;
 
         default:
-            res.setHeader("ContentType", "application/json");
-            // res.status(200).json({ club: club, approvals: approvals, joinbutton: joinbutton, verifypresident: verifypresident });
-            res.render('club',{ club, approvals, joinbutton, verifypresident });
+            res.render('club',{ club, approvals, joinbutton, verifypresident, message: req.flash("message"), status: req.flash("status") });
             break;
     }
 });
@@ -62,16 +67,20 @@ router.get("/:clubId/edit", async function(req,res,next){
     {
         var err = new Error("You are not logged in.");
         err.status = 400;
-        res.status(err.status).send(err.message);
-        return ;
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     if(Object.prototype.toString.call(club) === "[object Error]")
     {
-        if((club.status) < 500)
-        res.status(club.status).send(club.message);
-        else
-        next(club.message);
+        res.status(club.status)
+        req.flash("message", club.message );
+        req.flash("status", club.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
     else
     {
@@ -79,33 +88,37 @@ router.get("/:clubId/edit", async function(req,res,next){
         {
             var err = new Error("You are not president of club.");
             err.status = 400;
-            res.status(error.status).send(error.message); 
-            return ;
+            res.status(err.status)
+            req.flash("message", err.message );
+            req.flash("status", err.status);
+            res.redirect(`/club/${req.params.clubId}`);
+            return;
         }
     
-        res.setHeader("ContentType", "application/json");
-        // res.status(200).json({ message: "The club edit form will render here.", club: club });
-        res.render('update_club.ejs',{ message: "The club edit form will render here.", club: club });
     }
+    
+    res.render('update_club.ejs',{ club: club });
 
 });
 
-router.post("/:clubId", async function(req,res,next) {
+router.post("/:clubId", imageUpload.single("image"), async function(req,res,next) {
 
     const club = await putClub(req,res);
 
     if(Object.prototype.toString.call(club) === "[object Error]")
     {
-        if((club.status) < 500)
-        res.status(club.status).send(club.message);
-        else
-        next(club.message);
+        res.status(club.status)
+        req.flash("message", club.message );
+        req.flash("status", club.status);
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).send("The club is updated successfully.");
+        res.status(200)
+        req.flash("message", "The club is updated successfully." );
+        req.flash("status", 200);
     }
+
+    res.redirect(`/club/${req.params.clubId}`);
 });
 
 router.get("/:clubId/event", async function (req, res, next) {
@@ -113,7 +126,11 @@ router.get("/:clubId/event", async function (req, res, next) {
     if (req.session.passport === undefined) {
         var err = new Error("You are not logged in.");
         err.status = 400;
-        return res.status(err.status).send(err.message);
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     const { clubId } = req.params;
@@ -121,7 +138,11 @@ router.get("/:clubId/event", async function (req, res, next) {
     if (!mongoose.Types.ObjectId.isValid(clubId)) {
         var err = new Error("The Club doesn't exsist.");
         err.status = 406;
-        return res.status(err.status).send(err.message);
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     var club;
@@ -131,79 +152,111 @@ router.get("/:clubId/event", async function (req, res, next) {
 
     } catch (error) {
         error.message = "Unable to connect with database.";
-        res.status(error.status).send(error.message);
+        res.status(error.status)
+        req.flash("message", error.message );
+        req.flash("status", error.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     if (club === null) {
         var err = new Error("The Club doesn't exsist.");
         err.status = 406;
-        return res.status(err.status).send(err.message);
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     if (club.presidentid != req.session.passport.user) {
         var err = new Error("You are not president of club.");
         err.status = 400;
-        return res.status(err.status).send(err.message);
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect(`/club/${req.params.clubId}`);
+        return;
     }
 
     res.status(200).render('addEvent', { club });
-    //res.status(200).send("The event creation form will render here.")
 });
 
-router.post("/:clubId/event", upload.single("banner"), async function (req, res, next) {
+router.post("/:clubId/event", imageUpload.single("image"), async function (req, res, next) {
 
     const event = await postEvent(req, res);
 
     if(Object.prototype.toString.call(event) === "[object Error]")
     {
-        if((event.status) < 500)
-        res.status(event.status).send(event.message);
-        else
-        next(event.message);
+        res.status(event.status)
+        req.flash("message", event.message );
+        req.flash("status", event.status);
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).json(event);
+        res.status(200)
+        req.flash("message", "The Event is created Successfully." );
+        req.flash("status", 200);
     }
+
+    res.redirect(`/club/${req.params.clubId}`);
 
 });
 
-router.get("/:clubId/approval", async function(req,res,next) {
+router.post("/:clubId/approval", async function(req,res,next) {
 
     const approval = await postApproval(req, res);
 
-    if (Object.prototype.toString.call(approval) === "[object Error]") {
-        if ((approval.status) < 500)
-        res.status(approval.status).send(approval.message);
-        else
-        next(approval.message);
+    if (Object.prototype.toString.call(approval) === "[object Error]") 
+    {
+        res.status(approval.status)
+        req.flash("message", approval.message );
+        req.flash("status", approval.status);
     }
-    else {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).send("Your approval has been submitted successfully.");
+    else 
+    {
+        res.status(200)
+        req.flash("message", "Your approval has been submitted successfully." );
+        req.flash("status", 200);
     }
+
+    res.redirect(`/club/${req.params.clubId}`);
 
 });
 
-router.get("/:clubId/remove/:studentId", async function(req,res,next) {
+router.post("/:clubId/remove/:studentId", async function(req,res,next) {
 
     const member = await removeMember(req,res);
 
     if(Object.prototype.toString.call(member) === "[object Error]")
     {
-        if((member.status) < 500)
-        res.status(member.status).send(member.message);
-        else
-        next(member.message);
+        res.status(member.status)
+        req.flash("message", member.message );
+        req.flash("status", member.status);
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).send("The member has been removed successfully.");
+        res.status(200)
+        req.flash("message", "The member has been removed successfully." );
+        req.flash("status", 200);
 
-        // mail to member.
+        var mailOptions = {
+            from: process.env.usern,
+            to: member.student.email,
+            subject: `Fired from ${member.club.name}`,
+            text: `Thank you for being with us. Sorry ${member.student.name}, you got fired from ${member.club.name} Club.`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            error.message = "Unable to send mail right now.";
+            error.status = 500;
+            res.status(error.status)
+            req.flash("message", error.message );
+            req.flash("status", error.status);
+        });
     }
+
+    res.redirect(`/club/${req.params.clubId}`);
 
 });
 

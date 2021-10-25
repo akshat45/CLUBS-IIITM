8445555e8a7430f301c5f8f1e03e3afb48a3c9ab
Jay Raikhere <jayraykhere@gmail.com>
2021-10-25 12:01:33 +0530

    some changes in event.js

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,7 @@
 import express from "express";
-import eventModel from "../models/events.js";
 import { getEvent, putEvent, delEvent } from "../controllers/events.js";
+import imageUpload from "../middleware/imageUpload.js";
+import clubModel from "../models/clubs.js";
 
 const router = express.Router();
 
@@ -10,78 +11,132 @@ router.get("/:eventId", async function(req,res,next) {
 
     if(Object.prototype.toString.call(event) === "[object Error]")
     {
-        if((event.status) < 500)
-        res.status(event.status).send(event.message);
-        else
-        next(event.message);
+        res.status(event.status)
+        req.flash("message", event.message );
+        req.flash("status", event.status);
+        res.redirect("/home");
+        return ;
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).json(event);
+        var ispresident = false;
+        var club;
+        try {
+            club = await clubModel.findOne({ eventids: { $elemMatch: { $eq: event._id } } });
+            
+        } catch (error) {
+            error.message = "Unable to connect to database."
+            res.status(error.status)
+            req.flash("message", error.message );
+            req.flash("status", error.status);
+            res.redirect(`/event/${event._id}`);
+            return ;         
+        }
+
+        if(club.presidentid == req.session.passport.user)
+        {
+            ispresident = true;
+        }
+
+        res.status(200).render('events', { event, ispresident, message: req.flash("message"), status: req.flash("status") });
     }
 });
 
 router.get("/:eventId/edit", async function(req,res,next){
 
+    if (req.session.passport === undefined) {
+        var err = new Error("You are not logged in.");
+        err.status = 400;
+        res.status(err.status)
+        req.flash("message", err.message );
+        req.flash("status", err.status);
+        res.redirect("/home");
+        return ;
+    }
+
     const event = await getEvent(req,res);
 
     if(Object.prototype.toString.call(event) === "[object Error]")
     {
-        if((event.status) < 500)
-        res.status(event.status).send(event.message);
-        else
-        next(event.message);
+        res.status(event.status)
+        req.flash("message", event.message );
+        req.flash("status", event.status);
+        res.redirect("/home");
+        return ;
     }
     else
     {
-        if(event.clubid.presidentid != req.session.passport.user)
+        var club;
+        try {
+            club = await clubModel.findOne({ eventids: { $elemMatch: { $eq: event._id } } });
+            
+        } catch (error) {
+            error.message = "Unable to connect to database."
+            res.status(error.status)
+            req.flash("message", error.message );
+            req.flash("status", error.status);
+            res.redirect(`/event/${event._id}`);
+            return ;           
+        }
+
+        if(club.presidentid != req.session.passport.user)
         {
             var err = new Error("You are not president of club.");
             err.status = 400;
-            res.status(error.status).send(error.message); 
+            res.status(err.status)
+            req.flash("message", err.message );
+            req.flash("status", err.status);
+            res.redirect(`/event/${event._id}`);
+            return ;
         }
-    
-        res.setHeader("ContentType", "application/json");
-        res.status(200).json({ message: "The event edit form will render here.", event: event });
+
+        res.status(200).render('edit_events',{ event: event });
     }
 
 });
 
-router.post("/:eventId", async function(req,res,next) {
+router.post("/:eventId", imageUpload.single("image"), async function(req,res,next) {
 
     const event = await putEvent(req,res);
 
     if(Object.prototype.toString.call(event) === "[object Error]")
     {
-        if((event.status) < 500)
-        res.status(event.status).send(event.message);
-        else
-        next(event.message);
+        res.status(event.status)
+        req.flash("message", event.message );
+        req.flash("status", event.status);
+        res.redirect("/home");
+        return ;
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).send("The event is updated successfully.");
+        res.status(200)
+        req.flash("message", "The event is updated successfully." );
+        req.flash("status", 200);
     }
+
+    res.redirect(`/event/${event._id}`);
 });
 
-router.delete("/:eventId", async function(req,res,next) {
+router.post("/:eventId/delete", async function(req,res,next) {
 
     const event = await delEvent(req,res);
 
     if(Object.prototype.toString.call(event) === "[object Error]")
     {
-        if((event.status) < 500)
-        res.status(event.status).send(event.message);
-        else
-        next(event.message);
+        res.status(event.status)
+        req.flash("message", event.message );
+        req.flash("status", event.status);
+        res.redirect("/home");
+        return ;
     }
     else
     {
-        res.setHeader("ContentType", "application/json");
-        res.status(200).send("The event is deleted successfully.");
+        res.status(200)
+        req.flash("message", "The event is deleted successfully." );
+        req.flash("status", 200);
     }
+
+    res.redirect("/home");
 });
 
 export default router;
